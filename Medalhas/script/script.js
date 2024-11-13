6292682a81a01d3cf1be8c465f5c7d4f8c20d12a
Igao2 <igor.dasilvacosta8@gmail.com>
2024-11-13 12:16:11 -0300

    Visual alterado

---
+++ Medalhas/script/script.js
@@ -0,0 +1,13 @@
+var med = document.getElementById("med")
+        var trof = document.getElementById("trof")
+        var fitas = document.getElementById("fitas")
+        med.addEventListener("click",()=>{
+            window.location.href = "medals.html"
+        })
+        trof.addEventListener("click",()=>
+    {
+        window.location.href="trofeus.html"
+    })
+    fitas.addEventListener("click",()=>{
+        window.location.href="fitas.html"
+    })

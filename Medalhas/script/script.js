2a8e802d21cd7bdfd8a9ae90ca4d681e113253f9
Igao2 <igor.dasilvacosta8@gmail.com>
2024-11-25 14:33:34 -0300

    Carrosséis adicionados'

--- Medalhas/script/script.js
+++ Medalhas/script/script.js
@@ -1,13 +1,65 @@
-var med = document.getElementById("med")
-        var trof = document.getElementById("trof")
-        var fitas = document.getElementById("fitas")
-        med.addEventListener("click",()=>{
-            window.location.href = "medals.html"
-        })
-        trof.addEventListener("click",()=>
+document.addEventListener("DOMContentLoaded",()=>{
+    var body = document.body.id;
+    if(body=="med")
+        {
+            const carousel = document.getElementById('medcarr');
+            const items = document.querySelectorAll('.fmedalha');
+            let currentIndex = 0;
+            var next = document.getElementById("next");
+            var prev = document.getElementById("prev");
+            
+            function updateCarousel() {
+                const offset = -currentIndex * 100; // Calcula o deslocamento com base no índice
+                carousel.style.transform = `translateX(${offset}%)`;
+            }
+            
+            
+            next.addEventListener("click",()=>{
+                
+                    currentIndex = (currentIndex + 1) % items.length; // Avança para o próximo slide
+                    updateCarousel();
+                
+            })
+                
+            prev.addEventListener("click",()=>{
+                
+                    currentIndex = (currentIndex - 1 + items.length) % items.length; 
+                    updateCarousel();
+                
+            })
+        }
+    if(body=="trof")
     {
-        window.location.href="trofeus.html"
-    })
-    fitas.addEventListener("click",()=>{
-        window.location.href="fitas.html"
-    })
+        const carousel = document.getElementById('trofcarr');
+        const items = document.querySelectorAll('.ftrofeu');
+        let currentIndex = 0;
+        var next = document.getElementById("next");
+        var prev = document.getElementById("prev");
+        function updateCarousel() {
+            const offset = -currentIndex * 100; 
+            carousel.style.transform = `translateX(${offset}%)`;
+        }
+        
+        
+        next.addEventListener("click",()=>{
+            
+                currentIndex = (currentIndex + 1) % items.length; 
+                updateCarousel();
+            
+        })
+            
+        prev.addEventListener("click",()=>{
+            
+                currentIndex = (currentIndex - 1 + items.length) % items.length; 
+                updateCarousel();
+            
+        })
+           
+            
+            
+    }
+    
+   
+})
+
+    

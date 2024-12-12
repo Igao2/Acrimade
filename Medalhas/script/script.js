f71ab7e71bd382cf76aae41c7452fd0e107fd7fe
Igao2 <igor.dasilvacosta8@gmail.com>
2024-12-12 14:59:38 -0300

    responsividade, e botões funcionais

--- Medalhas/script/script.js
+++ Medalhas/script/script.js
@@ -1,11 +1,25 @@
 document.addEventListener("DOMContentLoaded",()=>{
     var body = document.body.id;
     if(body=="index")
-    {
+    {    
+        const insta = document.querySelector('.insta');
+        const whats = document.querySelector('.whats');
         const menuToggle = document.querySelector('.menu-toggle');
         const menu = document.querySelector('.menu');
         const overlay = document.querySelector('.overlay');
-        
+        insta.addEventListener("click",()=>{
+          var insta = "https://www.instagram.com/acrimade_personalizados/";
+          window.open(insta,"_blank");
+        })
+        whats.addEventListener("click",()=>{
+          const Telefone = '553592170284';
+          const mensagem= 'Gostaria de conversar sobre seus produtos';
+
+          const whatsappLink = `https://api.whatsapp.com/send?phone=${Telefone}&text=${encodeURIComponent(mensagem)}`;
+
+          window.open(whatsappLink,"_blank")
+        })
+
         // Abre e fecha o menu
         menuToggle.addEventListener('click', () => {
           menu.classList.toggle('active');
@@ -33,10 +47,38 @@ document.addEventListener("DOMContentLoaded",()=>{
     }
     if(body=="med")
         {
-            const menuToggle = document.querySelector('.menu-toggle');
+        const insta = document.querySelector('.insta');
+        const whats = document.querySelector('.whats');
+        const menuToggle = document.querySelector('.menu-toggle');
         const menu = document.querySelector('.menu');
         const overlay = document.querySelector('.overlay');
-        
+        const carousel = document.getElementById('medcarr');
+        const items = document.querySelectorAll('.fmedalha');
+        let currentIndex = 0;
+        var next = document.getElementById("next");
+        var prev = document.getElementById("prev");
+        const but = document.querySelector('.pedirmedalhas');
+        insta.addEventListener("click",()=>{
+          var insta = "https://www.instagram.com/acrimade_personalizados/";
+          window.open(insta,"_blank");
+        })
+        whats.addEventListener("click",()=>{
+          const Telefone = '553592170284';
+          const mensagem= 'Gostaria de conversar sobre seus produtos';
+
+          const whatsappLink = `https://api.whatsapp.com/send?phone=${Telefone}&text=${encodeURIComponent(mensagem)}`;
+
+          window.open(whatsappLink,"_blank")
+        })
+
+        but.addEventListener("click",() =>{
+          const Telefone = '553592170284';
+          const mensagem= 'Gostaria de fazer um orçamento para compra de medalhas';
+
+          const whatsappLink = `https://api.whatsapp.com/send?phone=${Telefone}&text=${encodeURIComponent(mensagem)}`;
+
+          window.open(whatsappLink,"_blank")
+        })
         // Abre e fecha o menu
         menuToggle.addEventListener('click', () => {
           menu.classList.toggle('active');
@@ -48,11 +90,7 @@ document.addEventListener("DOMContentLoaded",()=>{
           menu.classList.remove('active');
           overlay.classList.remove('active');
         });
-            const carousel = document.getElementById('medcarr');
-            const items = document.querySelectorAll('.fmedalha');
-            let currentIndex = 0;
-            var next = document.getElementById("next");
-            var prev = document.getElementById("prev");
+            
             
             function updateCarousel() {
                 const offset = -currentIndex * 100; // Calcula o deslocamento com base no índice
@@ -76,27 +114,53 @@ document.addEventListener("DOMContentLoaded",()=>{
         }
     if(body=="trof")
     {
-        const menuToggle = document.querySelector('.menu-toggle');
-const menu = document.querySelector('.menu');
-const overlay = document.querySelector('.overlay');
+      const insta = document.querySelector('.insta');
+      const whats = document.querySelector('.whats');
+      const but = document.querySelector('.pedirtrofeus');
+      const menuToggle = document.querySelector('.menu-toggle');
+      const menu = document.querySelector('.menu');
+      const overlay = document.querySelector('.overlay');
+      const carousel = document.getElementById('trofcarr');
+      const items = document.querySelectorAll('.ftrofeu');
+      let currentIndex = 0;
+      var next = document.getElementById("next");
+      var prev = document.getElementById("prev");
+      
+        insta.addEventListener("click",()=>{
+          var insta = "https://www.instagram.com/acrimade_personalizados/";
+          window.open(insta,"_blank");
+        })
+        whats.addEventListener("click",()=>{
+          const Telefone = '553592170284';
+          const mensagem= 'Gostaria de conversar sobre seus produtos';
 
+          const whatsappLink = `https://api.whatsapp.com/send?phone=${Telefone}&text=${encodeURIComponent(mensagem)}`;
+
+          window.open(whatsappLink,"_blank")
+        })
+
+        but.addEventListener("click",() =>{
+          const Telefone = '553592170284';
+          const mensagem= 'Gostaria de fazer um orçamento para compra de troféus';
+
+          const whatsappLink = `https://api.whatsapp.com/send?phone=${Telefone}&text=${encodeURIComponent(mensagem)}`;
+
+          window.open(whatsappLink,"_blank")
+        })
+       
 // Abre e fecha o menu
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  overlay.classList.toggle('active');
-});
+        menuToggle.addEventListener('click', () => {
+          menu.classList.toggle('active');
+          overlay.classList.toggle('active');
+        });
 
-// Fecha o menu ao clicar fora
-overlay.addEventListener('click', () => {
-  menu.classList.remove('active');
-  overlay.classList.remove('active');
-});
+        // Fecha o menu ao clicar fora
+        overlay.addEventListener('click', () => {
+          menu.classList.remove('active');
+          overlay.classList.remove('active');
+        });
 
-        const carousel = document.getElementById('trofcarr');
-        const items = document.querySelectorAll('.ftrofeu');
-        let currentIndex = 0;
-        var next = document.getElementById("next");
-        var prev = document.getElementById("prev");
+        
         function updateCarousel() {
             const offset = -currentIndex * 100; 
             carousel.style.transform = `translateX(${offset}%)`;
@@ -119,6 +183,71 @@ overlay.addEventListener('click', () => {
            
             
             
+    }
+    if(body=="fitas")
+    {
+      const but = document.querySelector('.pedirfitas');
+      insta.addEventListener("click",()=>{
+        var insta = "https://www.instagram.com/acrimade_personalizados/";
+        window.open(insta,"_blank");
+      })
+      whats.addEventListener("click",()=>{
+        const Telefone = '553592170284';
+        const mensagem= 'Gostaria de conversar sobre seus produtos';
+
+        const whatsappLink = `https://api.whatsapp.com/send?phone=${Telefone}&text=${encodeURIComponent(mensagem)}`;
+
+        window.open(whatsappLink,"_blank")
+      })
+
+        but.addEventListener("click",() =>{
+          const Telefone = '553592170284';
+          const mensagem= 'Gostaria de fazer um orçamento para compra de fitas';
+
+          const whatsappLink = `https://api.whatsapp.com/send?phone=${Telefone}&text=${encodeURIComponent(mensagem)}`;
+
+          window.open(whatsappLink,"_blank")
+        })
+        const menuToggle = document.querySelector('.menu-toggle');
+        const menu = document.querySelector('.menu');
+        const overlay = document.querySelector('.overlay');
+
+// Abre e fecha o menu
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  overlay.classList.toggle('active');
+});
+
+// Fecha o menu ao clicar fora
+overlay.addEventListener('click', () => {
+  menu.classList.remove('active');
+  overlay.classList.remove('active');
+});
+
+        const carousel = document.getElementById("carfitas");
+        const items = document.querySelectorAll('.ffitas');
+        let currentIndex = 0;
+        var next = document.getElementById("next");
+        var prev = document.getElementById("prev");
+        function updateCarousel() {
+            const offset = -currentIndex * 100; 
+            carousel.style.transform = `translateX(${offset}%)`;
+        }
+        
+        
+        next.addEventListener("click",()=>{
+            
+                currentIndex = (currentIndex + 1) % items.length; 
+                updateCarousel();
+            
+        })
+            
+        prev.addEventListener("click",()=>{
+            
+                currentIndex = (currentIndex - 1 + items.length) % items.length; 
+                updateCarousel();
+            
+        })
     }
     
    
